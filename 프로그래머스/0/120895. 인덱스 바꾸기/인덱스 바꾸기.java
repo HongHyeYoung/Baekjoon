dd4e1e90d2caeb836cd1f6672cbf37c57d16f59d
hyeyong <103356641+HongHyeYoung@users.noreply.github.com>
2024-03-17 20:37:31 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        char c1 = my_string.charAt(num1);
+        char c2 = my_string.charAt(num2);
+        
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.setCharAt(num1, c2);
+        sb.setCharAt(num2, c1);
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
