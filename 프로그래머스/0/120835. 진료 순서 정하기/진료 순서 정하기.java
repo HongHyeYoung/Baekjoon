6246daabb9ee049f2fe76a7fced60de74aa5f95e
hyeyong <103356641+HongHyeYoung@users.noreply.github.com>
2024-03-18 17:08:10 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int size = emergency.length;
+        int[] answer = new int[size];
+        boolean[] used = new boolean[size];
+        for(int i = 0 ; i < size ; i++){
+            int index = -1;
+            for(int j = 0 ; j < size ; j++){
+                if(used[j]){
+                    continue;
+                }
+                if(index == -1){
+                    index = j;
+                    continue;
+                }
+                if(emergency[index] < emergency[j]){
+                    index = j;
+                }
+            }
+            used[index] = true;
+            answer[index] = i+1;
+        }
+        return answer;
+    }
+}
